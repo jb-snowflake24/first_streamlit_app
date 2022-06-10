77f79d1281b8a8a5bd28ded0d64e9b1bdfcdc310
jb-snowflake24 <107212305+jb-snowflake24@users.noreply.github.com>
2022-06-09 20:42:22 -0400

    Update streamlit_app.py
    Added preselection of Avocados and Strawberries.

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 # Let's put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 #display the table on the page
 streamlit.dataframe(my_fruit_list)
+
+
